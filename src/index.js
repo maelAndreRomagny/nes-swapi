789cc4a197cfaefe37aec917060970549e8a8c41
maelAndreRomagny <49150157+maelAndreRomagny@users.noreply.github.com>
2019-06-03 16:46:06 +0200

    clarification code
    conditions de chargement ou aucun résultat trouvé
    externalisation de la fonction de recherche

--- src/index.js
+++ src/index.js
@@ -40,69 +40,83 @@ function Character({ character, heartFull, onHeartClicked }) {
 
 const toggle = b => !b;
 
+function searchSWApi(inputText) {
+  //Ne semble pas fonctionner
+  return fetch(`https://swapi.co/api/people/?search=${inputText}/`).then(
+    response => (response.status === 200 ? response.json() : null)
+  );
+}
+
 class HeartToggle extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      textInput: "",
       character: null,
-      heartFull: false,
-      randInt: 0,
-      randMin: 1,
-      randMax: 88
+      heartFull: false
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
-
-  componentDidMount() {
-    this.fillInfo();
-    this.onHeartClick();
-  }
-
-  searchChar(text) {}
-
-  generateNumber = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
 
   onHeartClick() {
     this.setState(state => ({
       heartFull: toggle(state.heartFull)
     }));
-    this.fillInfo();
-  }
-  //88 personnages (perso random)
-  getCharFromSWAPI() {
-    //Mauvaise méthode
-    this.state.randInt = this.generateNumber(
-      this.state.randMin,
-      this.state.randMax
-    );
-
-    return fetch(
-      "https://swapi.co/api/people/" + this.state.randInt + "/"
-    ).then(function(response) {
-      return response.json();
-    });
   }
 
-  fillInfo() {
-    this.getCharFromSWAPI()
-      .then(character => this.setState({ character: character }))
-      .catch(error => console.error(error));
+  fillInfo = () => {
+    this.setState({ notFound: false, loading: true });
+    searchSWApi(this.state.textInput)
+      .then(character =>
+        character
+          ? this.setState({ character: character }) //Ne transfert pas le tableau de données
+          : this.setState({ notFound: true })
+      )
+      .catch(error => console.error(error))
+      .finally(() => this.setState({ loading: false }));
+    console.log(this.state.character);
+  };
+
+  handleChange(event) {
+    this.setState({ textInput: event.target.value });
   }
 
   render() {
     return (
       <React.Fragment>
-        {!this.state.character ? (
-          <h2>Loading...</h2>
-        ) : (
+        {this.state.loading && <h3>Recherche en cours...</h3>}
+        {/*&& équivalent $$$ ? $$$$ : null*/}
+        {this.state.notFound && <h3>Non trouvé</h3>}
+        {this.state.character && (
           <Character
             heartFull={this.state.heartFull}
             onHeartClicked={() => this.onHeartClick()}
             character={this.state.character}
           />
         )}
+        <div>
+          <input
+            type="text"
+            id="search_field"
+            className="nes-input"
+            placeholder="Rechercher un personnage"
+            onChange={this.handleChange}
+            onKeyUp={e => {
+              if (e.keyCode === 13) {
+                e.preventDefault();
+                this.fillInfo();
+              }
+            }}
+          />
+          <button
+            onClick={() => this.fillInfo()}
+            disabled={this.state.textInput.length === 0 || this.state.loading}
+          >
+            Rechercher
+          </button>
+        </div>
       </React.Fragment>
     );
   }
